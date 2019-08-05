90efabe0fde3ac2b82256f4bcae34905f42cf115
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-08-05 16:19:12 -0700

    Enhance pygithubctl to enable/disable SSL verification

--- pygithubctl/pygithubctl.py
+++ pygithubctl/pygithubctl.py
@@ -24,6 +24,7 @@ import argparse
 import base64
 import os
 import logging
+import errno
 
 from github import Github
 from github import GithubException
@@ -32,7 +33,7 @@ from configurer import configure_logging
 logger = configure_logging(logging.getLogger('pygithubctl'))
 
 
-def download(repository, sha, source, target):
+def download_file(repository, sha, source, target):
     logger.info('Fetching %s from %s', source, repository)
     try:
         contents = repository.get_contents(source, ref=sha)
@@ -42,6 +43,27 @@ def download(repository, sha, source, target):
         output.close()
     except (GithubException, IOError) as exception:
         logging.error('Error processing %s: %s', source, exception)
+
+
+def download_directory(repository, sha, source, target):
+    try:
+        contents = repository.get_dir_contents(source, ref=sha)
+        for content in contents:
+            logger.info("Processing %s", content.path)
+            if content.type == 'dir':
+                download_directory(repository, sha, content.path, target)
+            else:
+                path = content.path
+                contents = repository.get_contents(path, ref=sha)
+                data = base64.b64decode(contents.content)
+                destination = os.path.join(target, path)
+                logger.debug("destination %s", destination)
+                mkdirs(os.path.join(target, os.path.dirname(path)))
+                output = open(destination, "w")
+                output.write(data)
+                output.close()
+    except (GithubException, IOError) as exception:
+        logging.error('Error processing %s: %s', content.path, exception)
 
 
 def get_sha(repository, tag):
@@ -67,6 +89,16 @@ def resolve_target(source, target):
         return target
 
 
+def mkdirs(path):
+    try:
+        os.makedirs(path)
+    except OSError as err:
+        if err.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
+
+
 def get_options():
     parser = argparse.ArgumentParser()
     parser.add_argument('--hostname', required=True)
@@ -78,6 +110,7 @@ def get_options():
     parser.add_argument('--destination', required=True)
     parser.add_argument("--http_ssl_verify", type=str_to_bool, nargs='?', const=True, default=True,
                         help="Enable/Disable HTTP SSL Verification")
+    parser.add_argument('--content_type', required=True)
     args = parser.parse_args()
     return args
 
@@ -113,12 +146,12 @@ def main():
     options = get_options()
     base_url = get_base_url(options.hostname)
     branch_or_tag = get_branch_or_tag(options)
-    destination = resolve_target(options.file_path, options.destination)
 
     logger.info('base_url: %s', base_url)
     logger.info('branch_or_tag: %s', branch_or_tag)
-    logger.info('destination: %s', destination)
+
     logger.info('http_ssl_verify: %s', options.http_ssl_verify)
+    logger.info('content_type: %s', options.content_type)
 
     github = Github(base_url=base_url, login_or_token=options.auth_token, verify=options.http_ssl_verify)
     organization = github.get_user().get_orgs()[0]
@@ -128,7 +161,15 @@ def main():
     sha = get_sha(repository, branch_or_tag)
     logger.info('sha: %s', sha)
 
-    download(repository, sha, options.file_path, destination)
+    if options.content_type.lower() in ('f', 'file'):
+        destination = resolve_target(options.file_path, options.destination)
+        logger.info('destination: %s', destination)
+        download_file(repository, sha, options.file_path, destination)
+    elif options.content_type.lower() in ('d', 'dir', 'directory'):
+        destination = options.destination
+        download_directory(repository, sha, options.file_path, destination)
+    else:
+        raise ValueError('Value of --content_type should be either file or directory')
 
 
 if __name__ == '__main__':
