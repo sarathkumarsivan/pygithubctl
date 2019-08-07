87289671441fa93b4ad8121896da247a160d2f65
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-08-06 23:34:19 -0700

    Code changes to download directory

--- setup.py
+++ setup.py
@@ -38,14 +38,14 @@ setup(name='pygithubctl',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
-          'Programming Language :: Python :: 2.7'
-          'Programming Language :: Python :: 3.0'
-          'Programming Language :: Python :: 3.1'
-          'Programming Language :: Python :: 3.2'
-          'Programming Language :: Python :: 3.3'
-          'Programming Language :: Python :: 3.4'
-          'Programming Language :: Python :: 3.5'
-          'Programming Language :: Python :: 3.6'
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.0',
+          'Programming Language :: Python :: 3.1',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7'
       ],
       keywords='pygithubctl githubctl',
