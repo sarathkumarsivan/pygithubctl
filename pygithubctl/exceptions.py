ddf1305823bf4eebcee2b9cb55e3950d0ff7499f
Sarath Kumar Sivan <sarathkumarsivan@gmail.com>
2019-08-17 21:00:55 -0700

    Exception Handling, code comments and feature enhancements

--- pygithubctl/exceptions.py
+++ pygithubctl/exceptions.py
@@ -20,3 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
+
+class PyGitHubCtlException(Exception):
+    """
+    Base class for other exceptions
+    """
+    pass
